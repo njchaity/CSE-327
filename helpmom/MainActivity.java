4ddbbf2a2c97e92d4302b4c6ff8058190f84c3f9
Foysal Ahmed <45089156+foysal-ahmed844@users.noreply.github.com>
2019-04-14 00:12:10 +0600

    Main Activity File

---
+++ helpmom/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.t_tiger.helpmom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // close splash activity
+        finish();
+
+    }
+}
