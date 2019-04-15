48af7a6a1e3794a28598e0e435074d2d51ddecf5
njchaity <nzchaity@gmail.com>
2019-04-15 22:08:13 +0600

    Add files via upload

---
+++ helpmom/Hospital.java
@@ -0,0 +1,13 @@
+package helpmom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital);
+    }
+}
